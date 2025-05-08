6964046b26fe6376205cb11774d7e3c2bb0d585c
Christian Tzurcanu <ctzurcanu@yahoo.com>
2025-05-08 13:15:36 +0200

    init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Applications/XAMPP/xamppfiles/htdocs/voting/node_modules/infima/dist/css/default/default.css"),
+  require("/Applications/XAMPP/xamppfiles/htdocs/voting/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Applications/XAMPP/xamppfiles/htdocs/voting/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Applications/XAMPP/xamppfiles/htdocs/voting/src/css/custom.css"),
+];
